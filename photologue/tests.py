b4343f586f9e2b68081dbcc82da19da905fbacba
justin.driscoll <justin.driscoll@2d8b3452-2338-0410-8a60-9550768eb463>
2008-08-27 20:06:16 +0000

    Reduced the size of the test images and updated tests.py. Added a new sample image.
    git-svn-id: http://django-photologue.googlecode.com/svn/trunk@294 2d8b3452-2338-0410-8a60-9550768eb463

--- photologue/tests.py
+++ photologue/tests.py
@@ -118,12 +118,12 @@ class ImageResizeTest(PLTest):
         self.assertEquals(self.ps.get_test_size(), (100, 100))
         
     def test_resize_to_fit_width_enlarge(self):
-        self.s.size = (2000, 0)
+        self.s.size = (400, 0)
         self.s.upscale = True
         self.s.save()
-        self.assertEquals(self.pl.get_test_size(), (2000, 1500))
-        self.assertEquals(self.pp.get_test_size(), (2000, 2667))
-        self.assertEquals(self.ps.get_test_size(), (2000, 2000))
+        self.assertEquals(self.pl.get_test_size(), (400, 300))
+        self.assertEquals(self.pp.get_test_size(), (450, 533))
+        self.assertEquals(self.ps.get_test_size(), (400, 400))
 
     def test_resize_to_fit_height(self):
         self.s.size = (0, 100)
@@ -133,12 +133,12 @@ class ImageResizeTest(PLTest):
         self.assertEquals(self.ps.get_test_size(), (100, 100))
         
     def test_resize_to_fit_height_enlarge(self):
-        self.s.size = (0, 2000)
+        self.s.size = (0, 400)
         self.s.upscale = True
         self.s.save()
-        self.assertEquals(self.pl.get_test_size(), (2667, 2000))
-        self.assertEquals(self.pp.get_test_size(), (1500, 2000))
-        self.assertEquals(self.ps.get_test_size(), (2000, 2000))
+        self.assertEquals(self.pl.get_test_size(), (533, 400))
+        self.assertEquals(self.pp.get_test_size(), (300, 400))
+        self.assertEquals(self.ps.get_test_size(), (400, 400))
         
     def test_resize_and_crop(self):
         self.s.crop = True
@@ -163,43 +163,43 @@ class ImageResizeTest(PLTest):
         self.assertEquals(self.ps.get_test_size(), self.s.size)
         
     def test_resize_one_dimension_width(self):
-        self.s.size = (1500, 1200)
+        self.s.size = (100, 150)
         self.s.save()
-        self.assertEquals(self.pl.get_test_size(), (1500, 1125))
+        self.assertEquals(self.pl.get_test_size(), (100, 75))
         
     def test_resize_one_dimension_height(self):
-        self.s.size = (1600, 1100)
+        self.s.size = (200, 75)
         self.s.save()
-        self.assertEquals(self.pl.get_test_size(), (1467, 1100))
+        self.assertEquals(self.pl.get_test_size(), (100, 75))
         
     def test_resize_no_upscale(self):
-        self.s.size = (2000, 2000)
+        self.s.size = (1000, 1000)
         self.s.save()
-        self.assertEquals(self.pl.get_test_size(), (1600, 1200))
+        self.assertEquals(self.pl.get_test_size(), (200, 150))
         
     def test_resize_no_upscale_mixed_height(self):
-        self.s.size = (3200, 600)
+        self.s.size = (400, 75)
         self.s.save()
-        self.assertEquals(self.pl.get_test_size(), (800, 600))
+        self.assertEquals(self.pl.get_test_size(), (100, 75))
         
     def test_resize_no_upscale_mixed_width(self):
-        self.s.size = (800, 2400)
+        self.s.size = (100, 300)
         self.s.save()
-        self.assertEquals(self.pl.get_test_size(), (800, 600))
+        self.assertEquals(self.pl.get_test_size(), (100, 75))
         
     def test_resize_no_upscale_crop(self):
-        self.s.size = (2000, 2000)
+        self.s.size = (1000, 1000)
         self.s.crop = True
         self.s.save()
-        self.assertEquals(self.pl.get_test_size(), (2000, 2000))
+        self.assertEquals(self.pl.get_test_size(), (1000, 1000))
         
     def test_resize_upscale(self):
-        self.s.size = (2000, 2000)
+        self.s.size = (1000, 1000)
         self.s.upscale = True
         self.s.save()
-        self.assertEquals(self.pl.get_test_size(), (2000, 1500))
-        self.assertEquals(self.pp.get_test_size(), (1500, 2000))
-        self.assertEquals(self.ps.get_test_size(), (2000, 2000))
+        self.assertEquals(self.pl.get_test_size(), (1000, 750))
+        self.assertEquals(self.pp.get_test_size(), (750, 1000))
+        self.assertEquals(self.ps.get_test_size(), (1000, 1000))
 
 
 class PhotoEffectTest(PLTest):
