f9d4ebb0a448a30a59b1bcb32649ad248b8a13d5
justin.driscoll <justin.driscoll@2d8b3452-2338-0410-8a60-9550768eb463>
2009-08-22 13:38:40 +0000

    Updated setup.py
    git-svn-id: http://django-photologue.googlecode.com/svn/trunk@402 2d8b3452-2338-0410-8a60-9550768eb463

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
+#/usr/bin/env python
 import os
-from distutils.command.install import INSTALL_SCHEMES
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-#http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
 
 # Dynamically calculate the version based on photologue.VERSION
 version_tuple = __import__('photologue').VERSION
@@ -18,31 +12,18 @@ if len(version_tuple) == 3:
 else:
     version = "%d.%d" % version_tuple[:2]
 
-# Scan for and add any data files    
-data_files = []
-
-for dirpath, dirnames, filenames in os.walk(SOURCE_DIR):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        continue
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
 setup(
-    name = "photologue",
+    name = "django-photologue",
     version = version,
     description = "Powerful image management for the Django web framework.",
     author = "Justin Driscoll",
     author_email = "justin.driscoll@gmail.com",
     url = "http://code.google.com/p/django-photologue/",
-    packages = ['photologue',
-                'photologue.management',
-                'photologue.management.commands',
-                'photologue.templatetags',
-                'photologue.utils'],
-    data_files = data_files,
+    packages=find_packages(),
+    # Make setuptools include all data files under version control,
+    # svn and CVS by default
+    include_package_data=True,
+    zip_safe=False,
     classifiers = ['Development Status :: 5 - Production/Stable',
                    'Environment :: Web Environment',
                    'Framework :: Django',
