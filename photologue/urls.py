965de2e3e1e17eddeb63506f0f5a08312db59d19
Marcos Daniel Petry <marcospetry@gmail.com>
2011-09-27 01:48:00 -0300

    change all url/views to class base views

--- photologue/urls.py
+++ photologue/urls.py
@@ -1,41 +1,53 @@
-from django.conf import settings
 from django.conf.urls.defaults import *
-from django.views.generic.detail import DetailView
-from models import *
-from django.views.generic.list import ListView
+from photologue.views import PhotoListView, PhotoDetailView, GalleryListView, \
+    GalleryDetailView, PhotoArchiveIndexView, PhotoDateDetailView, PhotoDayArchiveView, \
+    PhotoYearArchiveView, PhotoMonthArchiveView, GalleryArchiveIndexView, GalleryYearArchiveView, \
+    GalleryDateDetailView, GalleryDayArchiveView, GalleryMonthArchiveView
 
-# Number of random images from the gallery to display.
-from photologue.views import PhotoListView, PhotoDetailView, GalleryListView, GalleryDetailView
 
-SAMPLE_SIZE = ":%s" % getattr(settings, 'GALLERY_SAMPLE_SIZE', 5)
+urlpatterns = patterns('',
 
-# galleries
-gallery_args = {'date_field': 'date_added', 'allow_empty': True, 'queryset': Gallery.objects.filter(is_public=True), 'extra_context':{'sample_size':SAMPLE_SIZE}}
-urlpatterns = patterns('django.views.generic.date_based',
-    url(r'^gallery/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[\-\d\w]+)/$', 'object_detail', {'date_field': 'date_added', 'slug_field': 'title_slug', 'queryset': Gallery.objects.filter(is_public=True), 'extra_context':{'sample_size':SAMPLE_SIZE}}, name='pl-gallery-detail'),
-    url(r'^gallery/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', 'archive_day', gallery_args, name='pl-gallery-archive-day'),
-    url(r'^gallery/(?P<year>\d{4})/(?P<month>[a-z]{3})/$', 'archive_month', gallery_args, name='pl-gallery-archive-month'),
-    url(r'^gallery/(?P<year>\d{4})/$', 'archive_year', gallery_args, name='pl-gallery-archive-year'),
-    url(r'^gallery/?$', 'archive_index', gallery_args, name='pl-gallery-archive'),
-)
-
-# photographs
-photo_args = {'date_field': 'date_added', 'allow_empty': True, 'queryset': Photo.objects.filter(is_public=True)}
-urlpatterns += patterns('django.views.generic.date_based',
-    url(r'^photo/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[\-\d\w]+)/$', 'object_detail', {'date_field': 'date_added', 'slug_field': 'title_slug', 'queryset': Photo.objects.filter(is_public=True)}, name='pl-photo-detail'),
-    url(r'^photo/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', 'archive_day', photo_args, name='pl-photo-archive-day'),
-    url(r'^photo/(?P<year>\d{4})/(?P<month>[a-z]{3})/$', 'archive_month', photo_args, name='pl-photo-archive-month'),
-    url(r'^photo/(?P<year>\d{4})/$', 'archive_year', photo_args, name='pl-photo-archive-year'),
-    url(r'^photo/$', 'archive_index', photo_args, name='pl-photo-archive'),
-)
-
-urlpatterns += patterns('',
-
+    url(r'^gallery/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[\-\d\w]+)/$',
+        GalleryDateDetailView.as_view(),
+        name='pl-gallery-detail'),
+    url(r'^gallery/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$',
+        GalleryDayArchiveView.as_view(),
+        name='pl-gallery-archive-day'),
+    url(r'^gallery/(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
+        GalleryMonthArchiveView.as_view(),
+        name='pl-gallery-archive-month'),
+    url(r'^gallery/(?P<year>\d{4})/$',
+        GalleryYearArchiveView.as_view(),
+        name='pl-gallery-archive-year'),
+    url(r'^gallery/$', 
+        GalleryArchiveIndexView.as_view(),
+        name='pl-gallery-archive'),
+                        
     url(r'^gallery/(?P<slug>[\-\d\w]+)/$', GalleryDetailView.as_view() , name='pl-gallery'),
     url(r'^gallery/page/(?P<page>[0-9]+)/$', GalleryListView.as_view(), name='pl-gallery-list'),
 
-    url(r'^photo/(?P<slug>[\-\d\w]+)/$', PhotoDetailView.as_view() , name='pl-photo'),
-    url(r'^photo/page/(?P<page>[0-9]+)/$', PhotoListView.as_view(), name='pl-photo-list'),
+    url(r'^photo/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[\-\d\w]+)/$',
+        PhotoDateDetailView.as_view(),
+        name='pl-photo-detail'),
+    url(r'^photo/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$',
+        PhotoDayArchiveView.as_view(),
+        name='pl-photo-archive-day'),
+    url(r'^photo/(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
+        PhotoMonthArchiveView.as_view(),
+        name='pl-photo-archive-month'),
+    url(r'^photo/(?P<year>\d{4})/$',
+        PhotoYearArchiveView.as_view(),
+        name='pl-photo-archive-year'),
+    url(r'^photo/$', 
+        PhotoArchiveIndexView.as_view(),
+        name='pl-photo-archive'),
+
+    url(r'^photo/(?P<slug>[\-\d\w]+)/$',
+        PhotoDetailView.as_view(),
+        name='pl-photo'),
+    url(r'^photo/page/(?P<page>[0-9]+)/$',
+        PhotoListView.as_view(),
+        name='pl-photo-list'),
 
 )
 
--- photologue/views.py
+++ photologue/views.py
@@ -1,29 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from django.conf import settings
+from django.views.generic.dates import ArchiveIndexView, DateDetailView, DayArchiveView, MonthArchiveView, YearArchiveView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from photologue.models import Photo, Gallery
 
+# Number of random images from the gallery to display.
+SAMPLE_SIZE = ":%s" % getattr(settings, 'GALLERY_SAMPLE_SIZE', 5)
+
 class PhotoView(object):
     queryset = Photo.objects.filter(is_public=True)
 
-
-class GalleryView(object):
-    queryset = Gallery.objects.filter(is_public=True)
-
-
 class PhotoListView(PhotoView, ListView):
     paginate_by = 20
-    allow_empty = True
-
 
 class PhotoDetailView(PhotoView, DetailView):
     slug_field = 'title_slug'
 
+class PhotoDateView(PhotoView):
+    date_field = 'date_added'
+
+class PhotoDateDetailView(PhotoDateView, DateDetailView):
+    slug_field = 'title_slug'
+
+class PhotoArchiveIndexView(PhotoDateView, ArchiveIndexView):
+    pass
+
+class PhotoDayArchiveView(PhotoDateView, DayArchiveView):
+    pass
+
+class PhotoMonthArchiveView(PhotoDateView, MonthArchiveView):
+    pass
+
+class PhotoYearArchiveView(PhotoDateView, YearArchiveView):
+    pass
+
+
+#gallery Views
+class GalleryView(object):
+    queryset = Gallery.objects.filter(is_public=True)
 
 class GalleryListView(GalleryView, ListView):
     paginate_by = 20
-    allow_empty = True
+
 
 class GalleryDetailView(GalleryView, DetailView):
     slug_field = 'title_slug'
+
+class GalleryDateView(GalleryView):
+    date_field = 'date_added'
+    extra_context = {'sample_size':SAMPLE_SIZE}
+
+class GalleryDateDetailView(GalleryDateView, DateDetailView):
+    slug_field = 'title_slug'
+
+class GalleryArchiveIndexView(GalleryDateView, ArchiveIndexView):
+    pass
+
+class GalleryDayArchiveView(GalleryDateView, DayArchiveView):
+    pass
+
+class GalleryMonthArchiveView(GalleryDateView, MonthArchiveView):
+    pass
+
+class GalleryYearArchiveView(GalleryDateView, YearArchiveView):
+    pass
