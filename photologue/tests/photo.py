adf3a15cbbf2dd650975868802115287d9c569ea
Richard Barran <richard@arbee-design.co.uk>
2013-01-09 23:29:02 +0000

    Replace PIL (Python Imaging Library) with Pillow.

--- photologue/tests/photo.py
+++ photologue/tests/photo.py
@@ -1,8 +1,7 @@
-import Image
 import os
 from django.conf import settings
 from django.core.files.base import ContentFile
-from photologue.models import Photo, PHOTOLOGUE_DIR
+from photologue.models import Image, Photo, PHOTOLOGUE_DIR
 from photologue.tests.helpers import LANDSCAPE_IMAGE_PATH, PhotologueBaseTest, \
 QUOTING_IMAGE_PATH
 
