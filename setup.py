57797d49336d296c67c7f20402c7594863ffe939
Benjamin Liles <benliles@arch.tamu.edu>
2013-04-25 17:22:20 -0500

    Version metadata for 2.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
 
-version = '2.5'
+version = '2.5.1'
 
 setup(
     name="django-photologue",
