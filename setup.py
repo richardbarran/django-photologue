812b0c40e6032adb83413cb73476f2d5f53a58e7
Richard Barran <richard@arbee-design.co.uk>
2013-05-19 23:14:58 +0100

    Numbering of 2.6.1 release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
 
-version = '2.6'
+version = '2.6.1'
 
 
 def get_required_packages(requirements):
