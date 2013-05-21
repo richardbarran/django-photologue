6d04b0acfccbacf069e8cbde4217e5ac5e2585fd
Xavier Ordoquy <xordoquy@linovia.com>
2013-05-21 23:42:46 +0200

    Added the get_photo_model

--- photologue/__init__.py
+++ photologue/__init__.py
@@ -1,1 +1,22 @@
 
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_photo_model():
+    "Return the Photo model that is active in this project"
+    from django.conf import settings
+    from django.db.models import get_model
+
+    try:
+        CUSTOM_PHOTO_MODEL = settings.CUSTOM_PHOTO_MODEL
+    except AttributeError:
+        CUSTOM_PHOTO_MODEL = 'photologue.Photo'
+
+    try:
+        app_label, model_name = CUSTOM_PHOTO_MODEL.split('.')
+    except ValueError:
+        raise ImproperlyConfigured("CUSTOM_PHOTO_MODEL must be of the form 'app_label.model_name'")
+    photo_model = get_model(app_label, model_name)
+    if photo_model is None:
+        raise ImproperlyConfigured("CUSTOM_PHOTO_MODEL refers to model '%s' that has not been installed" % CUSTOM_PHOTO_MODEL)
+    return photo_model
