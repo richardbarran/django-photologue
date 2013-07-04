ff25c87b3ec4ce1d19bf81b833500464401e66ec
Richard Barran <richard@arbee-design.co.uk>
2013-07-04 20:50:24 +0200

    Put view classes in (roughly) same order as they're found in urls.py.

--- photologue/views.py
+++ photologue/views.py
@@ -5,6 +5,37 @@ from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from photologue.models import Photo, Gallery
 
+# Gallery views.
+
+class GalleryView(object):
+    queryset = Gallery.objects.filter(is_public=True)
+
+class GalleryListView(GalleryView, ListView):
+    paginate_by = 20
+
+class GalleryDetailView(GalleryView, DetailView):
+    slug_field = 'title_slug'
+
+class GalleryDateView(GalleryView):
+    date_field = 'date_added'
+
+class GalleryDateDetailView(GalleryDateView, DateDetailView):
+    slug_field = 'title_slug'
+
+class GalleryArchiveIndexView(GalleryDateView, ArchiveIndexView):
+    pass
+
+class GalleryDayArchiveView(GalleryDateView, DayArchiveView):
+    pass
+
+class GalleryMonthArchiveView(GalleryDateView, MonthArchiveView):
+    pass
+
+class GalleryYearArchiveView(GalleryDateView, YearArchiveView):
+    pass
+
+# Photo views.
+
 class PhotoView(object):
     queryset = Photo.objects.filter(is_public=True)
 
@@ -33,30 +64,3 @@ class PhotoYearArchiveView(PhotoDateView, YearArchiveView):
     pass
 
 
-#gallery Views
-class GalleryView(object):
-    queryset = Gallery.objects.filter(is_public=True)
-
-class GalleryListView(GalleryView, ListView):
-    paginate_by = 20
-
-class GalleryDetailView(GalleryView, DetailView):
-    slug_field = 'title_slug'
-
-class GalleryDateView(GalleryView):
-    date_field = 'date_added'
-
-class GalleryDateDetailView(GalleryDateView, DateDetailView):
-    slug_field = 'title_slug'
-
-class GalleryArchiveIndexView(GalleryDateView, ArchiveIndexView):
-    pass
-
-class GalleryDayArchiveView(GalleryDateView, DayArchiveView):
-    pass
-
-class GalleryMonthArchiveView(GalleryDateView, MonthArchiveView):
-    pass
-
-class GalleryYearArchiveView(GalleryDateView, YearArchiveView):
-    pass
