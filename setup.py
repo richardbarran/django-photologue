cdfc8a8c88ca63c547d87c171ba93d01cf773e3b
Richard Barran <richard@arbee-design.co.uk>
2014-09-16 22:27:10 +0100

    Package management in Python is as clear as mud.

--- setup.py
+++ setup.py
@@ -24,14 +24,7 @@ setup(
     author_email="justin@driscolldev.com, marcospetry@gmail.com, richard@arbee-design.co.uk",
     url="https://github.com/jdriscoll/django-photologue",
     packages=find_packages(),
-    package_data={
-        'photologue': [
-            'res/*.jpg',
-            'locale/*/LC_MESSAGES/*',
-            'templates/photologue/*.html',
-            'templates/photologue/tags/*.html',
-        ]
-    },
+    include_package_data=True,
     zip_safe=False,
     classifiers=['Development Status :: 5 - Production/Stable',
                  'Environment :: Web Environment',
