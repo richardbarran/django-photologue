35f3b9a2241986466cfaa9ba8fae5ec61811f85d
Richard Barran <richard@arbee-design.co.uk>
2014-10-27 09:55:18 +0100

    Couple of bugfixes/tweaks to example project.

--- example_project/example_project/static/js/project.js
+++ example_project/example_project/static/js/project.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
 	var path = window.location['pathname'];
 	if (path == '/') {
 		$('ul.navbar-nav li.home').addClass('active');
-	} else if (path.substring(0, 20) == '/photologue/gallery/') {
+	} else if (path.substring(0, 19) == '/photologue/gallery') {
 		$('ul.navbar-nav li.galleries').addClass('active');
-	} else if (path.substring(0, 18) == '/photologue/photo/') {
+	} else if (path.substring(0, 17) == '/photologue/photo') {
 		$('ul.navbar-nav li.photos').addClass('active');
 	}
 })
