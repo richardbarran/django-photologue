d5ff4647745c47fd855515410ea949caad964852
Richard Barran <richard@arbee-design.co.uk>
2021-10-02 22:01:08 +0200

    Add flake8 to CI.

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -4,8 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('photologue/', include('photologue.urls')),
-                  path('', TemplateView.as_view(template_name="homepage.html"), name='homepage'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [path('admin/', admin.site.urls),
+               path('photologue/', include('photologue.urls')),
+               path('', TemplateView.as_view(template_name="homepage.html"), name='homepage'),
+               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
